50d24bc857acc28d2310d5b22d4a32dad1bae117
csnoboa <caique2noboa@gmail.com>
2021-04-07 08:14:24 -0300

    changes in class transaction

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -5,24 +5,40 @@ class Transaction {
   final String title;
   final double value;
   final String date;
+  String description;
+  bool monthly;
+  int installments;
+  int totalInstallments;
 
   Transaction({
     @required this.id,
     @required this.title,
     @required this.value,
     @required this.date,
+    this.description = " ",
+    this.monthly = false,
+    this.installments = 1,
+    this.totalInstallments = 1,
   });
 
   Transaction.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         title = json['title'],
         value = json['value'],
-        date = json['date'];
+        date = json['date'],
+        description = json['description'],
+        monthly = json['monthly'],
+        installments = json['installments'],
+        totalInstallments = json['totalInstallments'];
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'title': title,
         'value': value,
         'date': date,
+        'description': description,
+        'monthly': monthly,
+        'installments': installments,
+        'totalInstallments': totalInstallments,
       };
 }
