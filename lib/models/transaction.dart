5ca6a87403bd07ad9ba7443dff10d3c8b97d7345
csnoboa <caique2noboa@gmail.com>
2021-04-07 10:13:17 -0300

    comments in transaction class

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/foundation.dart';
 
+///Class Transaction
+///The transaction class represents a note that has the following arguments:
+/// ```dart
+/// - @required String id: unique Transaction identifier
+///
+///
+/// - @required String title: Transaction title
+///
+/// - @required double value: Transaction value
+/// ```
+///   (negative for expenses and positive for revenues)
+///
+///```dart
+///
+/// - @required String date: date the Transaction was made
+///
+/// - String description: Transaction description
+///     - default: " "
+///
+/// - bool completed: true if the transaction has been received / paid, and false otherwise
+///    - default: false
+///
+/// - bool monthly: true if it is a bill with a monthly charge,
+/// it will generate several bills with the same day in all months ahead.
+/// The value of each note can be modified individually.
+///    - default: false
+///
+/// - int intallments: the parcel number of the note (1 if it is a single note)
+///    - default: 1
+///
+/// - int totalInstallments: the total number of installments (1 if it is a single note)
+///     - default: 1
+/// ```
 class Transaction {
   final String id;
   final String title;
   final double value;
   final String date;
   String description;
+  bool completed;
   bool monthly;
   int installments;
   int totalInstallments;
@@ -17,6 +51,7 @@ class Transaction {
     @required this.date,
     this.description = " ",
     this.monthly = false,
+    this.completed = false,
     this.installments = 1,
     this.totalInstallments = 1,
   });
@@ -28,6 +63,7 @@ class Transaction {
         date = json['date'],
         description = json['description'],
         monthly = json['monthly'],
+        completed = json['completed'],
         installments = json['installments'],
         totalInstallments = json['totalInstallments'];
 
@@ -38,6 +74,7 @@ class Transaction {
         'date': date,
         'description': description,
         'monthly': monthly,
+        'completed': completed,
         'installments': installments,
         'totalInstallments': totalInstallments,
       };
